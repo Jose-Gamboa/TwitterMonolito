dff447a6a6f8adca19e65d44945c1f49195ed4dd
unknown <jaime.cacuna@labinfo.is.escuelaing.edu.co>
2022-11-02 18:10:19 -0500

    Update Post

--- src/main/java/edu/eci/arep/TwitterMain.java
+++ src/main/java/edu/eci/arep/TwitterMain.java
@@ -6,15 +6,14 @@ import static spark.Spark.*;
 
 public class TwitterMain {
 
+    private static LogServiceImpl logService = new LogServiceImpl();
 
-    public static void main(String... args){
+    public static void main(String... args) {
         port(getPort());
         staticFileLocation("/public");
-        post("/twitter", (request, response) -> "hola");
-
+        post("/tweet", (request, response) -> getFront(request, response));
 
     }
-
 
     private static int getPort() {
         if (System.getenv("PORT") != null) {
@@ -23,14 +22,15 @@ public class TwitterMain {
         return 4567;
     }
 
-    private static void getFront(spark.Request request, spark.Response response) throws Exception {
-        String cadena = request.queryParams("tweet");
-        if(cadena.length() >= 140){
+    private static String getFront(spark.Request request, spark.Response response) throws Exception {
+        String tweet = request.queryParams("tweet");
+        System.out.println("Este es el tweet " + tweet);
+        if (tweet.length() >= 140) {
             throw new Exception("Uso maximo de caracteres es 140");
-        }else{
-
+        } else {
+            logService.insertDocument(tweet);
+            return logService.getCurrent();
         }
     }
-
 
 }
